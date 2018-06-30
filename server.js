e34d28cd89fe69191b841e81d6f47810dc0b9b79
dbmarch <dbmarch@gmail.com>
2018-06-30 18:15:01 -0500

    integrate functions into templates

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use (express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {return text.toUpperCase()})
 
 app.get('/', (req, res) =>  {
-    //res.send('<h1>Hello!!</h1>');
-    res.send({
-        name: 'Dave',
-        Pets: [
-            'Toby', 
-            'Sadie'
-        ]
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'This is the home page',
+    });
+    
 });
 
 app.get('/about', (req,res)=> {
- res.send('About Page');
+ res.render('about.hbs', {
+     pageTitle: 'About Page'
+ });
 });
 
 app.get ('/bad', (req,res) => {
